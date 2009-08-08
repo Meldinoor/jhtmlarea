75545dec353df1941dbe34160f49f66903e91b80
Chris Pietschmann <crpietschmann@codeplex.com>
2009-08-08 18:07:35 +0000

    Finished Work Item # 8396 & 8384

---
+++ jHtmlArea/jHtmlArea_Website/AutoPostTest.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class PostTest
+    Inherits System.Web.UI.Page
+
+    Public Sub PostTest_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        txtText.Value = Request("txtText")
+    End Sub
+End Class
